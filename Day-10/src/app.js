44bc0c8073a56d61df26c628cb05a6e3e293b3ae
Kritika Jain <kritika098jain@gmail.com>
2026-04-08 15:39:18 +0530

    Day-10 done

--- Day-10/src/app.js
+++ Day-10/src/app.js
@@ -2,11 +2,13 @@
 
 const express = require('express')
 const authRouter = require('./routes/auth.routes')
-
+const cookieParser = require('cookie-parser')
 
 
 const app = express()
 app.use(express.json())
+app.use(cookieParser())
+
 
 app.use('/api/auth',authRouter)
 // iska mtlb hai ki jab bhi koi request aayegi /api/auth se start hogi to usko authRouter ke pass bhej do jaha pe humne register ka route banaya hai
--- Day-10/src/routes/auth.routes.js
+++ Day-10/src/routes/auth.routes.js
@@ -2,6 +2,8 @@
 
 const express = require('express')
 const userModel = require('../models/user.model')
+const jwt = require('jsonwebtoken')
+
 
 const authRouter = express.Router()
 // agr hume app.js ke alva khi api create karni hai to hume express.Router() ka use krna hota hai
@@ -9,13 +11,36 @@ const authRouter = express.Router()
 authRouter.post('/register',async(req,res)=>{
     const {name,email,password} = req.body
     // yha pe hum user se name, email aur password lenge
-    userModel.create({
+
+    const isUserAlreadyExist = await userModel.findOne({email})
+    if(isUserAlreadyExist){
+        return res.status(409).json({
+            message : "user already exist with this email"
+        })
+    }
+// sabse phale check karega ki same mail wala user already exist to nhi karta , agr larta hai to 409 status code ke sath error message bhej dega
+    
+    const user = await userModel.create({
         name,email,password
     })
+// agr nhi karta to user create hoyega 
+
+    const token = jwt.sign({
+        userId : user._id,
+    }
+    ,process.env.JWT_SECRET,)
+// uske baad hum ek token generate karenge jisme userId hoga aur secret key hogi jo ki .env file me store hai aur set kar denge token ko cookie me jisse ki user ko baar baar login na karna pade
+// kyoki user jitne baar bhi request karega to uske sath token bhi bhej dega jisse ki server ko pata chal jayega ki ye request kis user se aayi hai
+
+res.cookie("token", token)
+
 
     res.status(201).json({
         message : "user registered successfully", 
-        users
+        user,
+        token
     })
+// agr user successfully register ho jata hai to 201 status code ke sath success message, user data aur token bhej dega
+
 })
 module.exports = authRouter
